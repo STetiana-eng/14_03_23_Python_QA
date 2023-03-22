e5e367d3046c201ab5d3be41e5bd49568eb30c20
Tetiana <selentsova12@gmail.com>
2023-03-22 11:47:01 +0200

    HW2.1

--- 14_03_23_python-qa/HW2.py
+++ 14_03_23_python-qa/HW2.py
@@ -3,22 +3,9 @@
 #наприклад "Word 'Python' has 6 letters".
 #Для отримання слова для аналізу скористайтеся константою або функцією input().
 
-my_input = input ()
-my_string = "Word {} has {} letters".format(my_input, len(my_input))
+my_input = input ("")
+my_string = "Word \"{} \" has {} letters".format(my_input, len(my_input))
 print (my_string)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Напишіть программу "Касир в кінотеватрі", яка попросіть користувача ввести
 # свій вік (можно використати константу або функцію input(), на екран має бути 
@@ -28,22 +15,18 @@ print (my_string)
 # якщо користувачу більше 65 - вивести повідомлення"Покажіть пенсійне посвідчення!"
 # якщо вік користувача містить цифру 7 - вивести повідомлення "Вам сьогодні пощастить!"
 # у будь-якому іншому випадку - вивести повідомлення "А білетів вже немає!"
- 
- 
-field = "Будь-ласка введіть свій вік:"
-print(field)
-age = input()
-result = int(age.find ("7"))
-if result > -1 :
- print ("Вам сьогодні пощастить!") 
-elif int(age) == 7 or int(age) > 7:
+
+
+age = input("Будь-ласка введіть свій вік:")
+if not age.isdigit ():
+  print ("Тільки цифри")
+elif age.find ("7") > -1:
+    print ("Вам сьогодні пощастить!") 
+elif int(age) > 0 and int(age) < 7:
  print ("Де твої батьки?")
-elif int(age) == 16 or int(age) > 16:
+elif int(age)  >= 7 and int(age) < 16:
  print ("Це фільм для дорослих!")
 elif int(age) >= 65:
  print ("Покажіть пенсійне посвідчення!")
 else:
  print ("А білетів вже немає!")
-           
-
-
