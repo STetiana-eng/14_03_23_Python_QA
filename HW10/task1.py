f0b675a125173cbf7e0d65634d1885e497589051
Tetiana <selentsova12@gmail.com>
2023-04-24 21:09:34 +0200

    add new tasks

---
+++ HW10/task1.py
@@ -0,0 +1,57 @@
+
+https://www.uatalents.com/
+
+
+# //div[@id="mm-1"]
+# //div[@class="job-search-form"]
+# //div[@class="title-box"]
+# //div[@class="text"]
+# //*[@class="mm-listitem__text"]
+# //*[@value="25/04/22"]
+# //*[@src="https://www.uatalents.com/images/avatar.png"]
+# //*[text()="Hybrid"]
+# //*[text()="Show 30"]
+# //*[text()="Show Filters"]
+# //*[@value="English Translator"]
+# //*[@href="https://blog.uatalents.com"]
+# //*[@class="icon-user"]
+# //*[@onclick="sign_in_apply_for_job()"]
+# //*[@type="text"]
+# //*[@class="pwd"]
+# //*[@rel="modal:close"]
+# //*[text()="Privacy"]
+# //*[@class="icon icon-calendar"]
+# //*[@class="mm-btn mm-btn_prev mm-navbar__btn"]
+# //*[text()=" Remember me"]
+# //*[text()="Forgot password?"]
+# //*[@id="login_64458ec4d5fe3"]
+# //*[@data-id="507"]
+# //*[@class="facebook"]
+# //*[@class="login-box"]
+# //*[@id="mm-0"]
+# //*[@class="select2-selection__rendered"]
+# //*[@class="item"][last()]
+# //*[@class="item"][1]
+# //*[@lang="en"]
+# input:default
+# [data - gtm - form - interact - field - id = "3"]
+# //input[@name ="s"]
+# //*[text()="Let the good job find you 🤙"]
+# a[href^="https"]
+# a[href="#"]
+# //*[@id="mm-0"]/footer/div[1]/div
+# //*[@id="navbar"]
+# // *[ @ id = "checkbox1"]
+# input: required, [name = "email"]
+# img[src^="https"]
+# link: not ([rel="icon])
+# input, section
+# [style="visibility: visible; animation-delay: 1000ms; animation-name: fadeInUp;"]
+# #navbar > li:nth-child(3) > a
+# [target="_blank"][href ^= "https://blog.uatalents.com"]
+# div.mobile-header
+# div + h4
+# //head/link[1][@type="image/png"]
+#
+#
+#
